5a4dc6678c3bdc3bd73c3213feda750c96c3dcb4
Randy Steele <steelerx155@gmail.com>
2019-09-30 20:24:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
    counter = 0
    while counter > array.length do
      
-     array.index(value_to_find[counter])
+     array.index(value_to_find)
      counter +=1
    end
    nil
