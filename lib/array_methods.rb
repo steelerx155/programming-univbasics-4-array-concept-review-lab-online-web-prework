4b3b4be32272110267ae8d0f4dfe820dc94fb12d
Randy Steele <steelerx155@gmail.com>
2019-09-30 20:37:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
    while counter < array.length do
      if array[counter] == value_to_find
      return counter
-   end
+    end
    counter += 1
 end
 nil
